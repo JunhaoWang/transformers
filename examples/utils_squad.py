a4c6098da8b80c788410a48bcf2e2e3014bc9201
Hao <junhao@umich.edu>
2019-11-03 19:47:11 -0500

    add parallel read example

--- examples/utils_squad.py
+++ examples/utils_squad.py
@@ -35,8 +35,6 @@ import pickle
 
 from joblib import Parallel, delayed
 
-PARALLEL_TQDM = None
-
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
